4f85bac43736f45ef0e2ce32f1508d7541533226
kimdongjooon <ehdwns6745@naver.com>
2023-09-25 20:09:27 +0900

    jblog controller

--- jblog03/src/main/java/com/poscodx/jblog/controller/BlogController.java
+++ jblog03/src/main/java/com/poscodx/jblog/controller/BlogController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/{id:(?!assets).*}") 
+@RequestMapping("/{id:^(?!assets$|user$|blog$).*}") 
 // 단 asset도 사용했을때도 들어와서 PathVariable옵션에서 제외시킬수있는 정규 표현식 찾기.
 public class BlogController {
 
@@ -22,11 +22,11 @@ public class BlogController {
 		System.out.println("categoryNo :"+ categoryNo);
 		System.out.println("postNo :"+ postNo);
 		
-		// 입력된 id가 회원db에서 조회시 없을때는 
+		// 입력된 id가 회원db에서 조회시 없을때는 404 에러 페이지.
 		
-		// 있을때는 홈페이지에 접속.
+		// 있을때는 그래도 통과.
 		
-		return "main/index";
+		return "blog/main";
 	}
 	
 	@ResponseBody
--- jblog03/src/main/java/com/poscodx/jblog/controller/UserController.java
+++ jblog03/src/main/java/com/poscodx/jblog/controller/UserController.java
@@ -1,8 +1,41 @@
 package com.poscodx.jblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.poscodx.jblog.vo.UserVo;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-
+	
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "user/join";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo userVo) {
+		return "user/joinsuccess";
+	}
+	
+	
+	
+	// 로그인, 로그아웃 Auth인터프리터 사용해서 세션으로 로그인기능 구현하기.
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login() {
+		return "user/login";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(UserVo userVo) {
+		return "user/login";
+	}
+	
+	
+	
+	
+	
+	
 }
