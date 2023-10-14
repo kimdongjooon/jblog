7f94bac87ff80660ae78fdf8cf0bff65d7ba56e6
kimdongjooon <ehdwns6745@naver.com>
2023-10-14 17:35:21 +0900

    jblog06 springboot 적용,

--- jblog05/src/main/java/com/poscodx/jblog/config/web/MessageSourceConfig.java
+++ jblog05/src/main/java/com/poscodx/jblog/config/web/MessageSourceConfig.java
@@ -1,11 +1,14 @@
 package com.poscodx.jblog.config.web;
 
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration 
 public class MessageSourceConfig {
+	
+	@Bean
 	public MessageSource messageSource() {
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.setBasenames("com/poscodx/jblog/config/web/messages/messages_ko","com/poscodx/jblog/config/web/messages/messages_en");
---
+++ jblog06/src/main/java/com/poscodx/jblog/config/WebConfig.java
@@ -0,0 +1,69 @@
+package com.poscodx.jblog.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import com.poscodx.jblog.interceptor.BlogInterceptor;
+import com.poscodx.jblog.security.AuthInterceptor;
+import com.poscodx.jblog.security.LoginInterceptor;
+import com.poscodx.jblog.security.LogoutInterceptor;
+
+@SpringBootConfiguration
+public class WebConfig implements WebMvcConfigurer {
+	//
+	// Security Interceptors
+	//
+	
+	@Bean
+	public HandlerInterceptor LoginInterceptor() {
+		return new LoginInterceptor();
+	}
+	
+	@Bean
+	public HandlerInterceptor LogoutInterceptor() {
+		return new LogoutInterceptor();
+	}
+	
+	@Bean
+	public HandlerInterceptor AuthInterceptor() {
+		return new AuthInterceptor();
+	}
+	
+	// BlogInterceptor
+	@Bean
+	public HandlerInterceptor blogInterceptor() {
+		return new BlogInterceptor();
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+			.addInterceptor(LoginInterceptor())
+			.addPathPatterns("/user/auth");
+		
+		registry
+			.addInterceptor(LogoutInterceptor())
+			.addPathPatterns("/user/logout","/blog/logout");
+		
+		registry
+			.addInterceptor(AuthInterceptor())
+			.addPathPatterns("/**")
+			.excludePathPatterns("/assets/**","/user/auth","/user/logout","/blog/logout");
+		
+		registry
+		.addInterceptor(blogInterceptor())
+		.addPathPatterns("/**")
+		.excludePathPatterns("/assets/**");
+	}
+	
+	
+		
+
+
+}
